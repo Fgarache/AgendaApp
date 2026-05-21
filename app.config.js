b3756dea39389385aed47651004e1dc5c0f7a7e1
Francisco Garache <franciscogarach3@gmail.com>
2026-05-21 14:58:41 -0600

    master(master)

--- app.config.js
+++ app.config.js
@@ -4,9 +4,11 @@ export default {
   expo: {
     name: "AgendaApp",
     slug: "agenda-app",
+    scheme: "agenda-app", // <--- ESTO SOLUCIONA EL WARN DE LINKING
     extra: {
       firebaseApiKey: process.env.FIREBASE_API_KEY,
       firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseDatabaseUrl: process.env.FIREBASE_DATABASE_URL, // <--- REVISA QUE DIGA SÓLO process.env
       firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
       firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
       firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
